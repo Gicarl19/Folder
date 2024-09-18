ff8131cf7059e81d24e8f416498140ed6ad1f2f8
Gicarl19 <giancarlo.sinday@gmail.com>
2024-09-18 19:35:05 +0800

    Add files via upload

---
+++ PROJECTSENNA/homepage.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Cebu Pacific Airline</title>
+       <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="BOX">
+           <ul type="none">
+            <li><a href="">Home</a></li>
+            <li><a href="">Ticket Reservation</a></li>
+            <li><a href="">Flight Reserve</a></li>
+            <li><a href="">About us</a></li>
+
+           </ul>
+        </nav>
+
+        </div>
+
+    </body>
+</html>
